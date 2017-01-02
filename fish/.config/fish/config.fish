85c12a0b6ac35d49eb324d929a37c3401441ad57
Max Salminen <max.salminen@spartaconsulting.fi>
2017-01-02 16:49:37 +0200

    Add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,15 @@
+# Path to Oh My Fish install.
+set -q XDG_DATA_HOME
+  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
+  or set -gx OMF_PATH "$HOME/.local/share/omf"
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG "/home/maxsal/.config/omf"
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
+source ~/.asdf/asdf.fish
+
+# Aliases
+alias ..='cd ..'
+alias ...='cd ../..'
