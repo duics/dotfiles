26276b0edc8165879f46425a2a052020b5f568e8
Max Salminen <max.salminen@protonmail.com>
2017-12-08 14:17:04 +0200

    Add lein confs

---
+++ lein/.lein/profiles.clj
@@ -0,0 +1,13 @@
+{:user {:plugins [
+                  ;; [lein-swank "1.4.5"]
+                  ;; [lein-pedantic "0.0.5"]
+                  ;; [lein-ritz "0.6.0"]
+                  [refactor-nrepl "2.3.1"]
+                  [cider/cider-nrepl "0.15.1"]
+                  ;; [lein-checkout-deps "1.0.0"]
+                  ;; [lein-haml-sass "0.2.4"]
+                  ;; [lein-midje "2.0.1"]
+                  ;; [lein-kibit "0.0.7"]
+                  ]}
+       {:dependencies [[slamhound "1.3.0"]]}
+{:aliases {"slamhound" ["run" "-m" "slam.hound"]}}}
