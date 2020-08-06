98e78d6910d9864bee1e1f95b578c8b2fb34334d
Max Salminen <max.salminen@smartly.io>
2020-08-06 12:59:54 +0300

    Update doom and add snippets

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -1,57 +1,63 @@
-;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Iosevka" :size 13))
-(setq css-indent-offset 2)
-(setq css-indent-level 2)
-(setq js-indent-level 2)
-(setq web-mode-css-indent-offset 2)
-(setq web-mode-code-indent-offset 2)
-(setq evil-shift-round nil)
-(setq evil-shift-width 2)
-(setq-default evil-shift-width 2)
-(setq-default indent-tabs-mode nil)
-(setq indent-tabs-mode nil)
-(setq tab-width 2)
-(global-whitespace-mode)
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
 
-(add-hook 'js2-mode-hook
-		  (lambda ()
-			(setq
-				js2-basic-offset 2
-				sgml-basic-offset 2
-				standard-indent 2
-				tab-width 2
-				indent-tabs-mode nil
-				js-indent-level 2
-				js2-basic-offset 2
-				js2-strict-semi-warning nil
-				js2-missing-semi-one-line-override nil
-				web-mode-markup-indent-offset 2
-				web-mode-css-indent-offset 2
-				web-mode-code-indent-offset 2
-				web-mode-indent-style 2
-				evil-shift-width 2
-				)))
 
-(add-hook 'rjsx-mode-hook
-		  (lambda ()
-			(setq
-				js2-basic-offset 2
-				sgml-basic-offset 2
-				standard-indent 2
-				tab-width 2
-				indent-tabs-mode nil
-				js-indent-level 2
-				js2-basic-offset 2
-				js2-strict-semi-warning nil
-				js2-missing-semi-one-line-override nil
-				web-mode-markup-indent-offset 2
-				web-mode-css-indent-offset 2
-				web-mode-code-indent-offset 2
-				web-mode-indent-style 2
-				evil-shift-width 2
-				)))
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Max Salminen"
+      user-mail-address "max.salminen@smartly.io")
 
-(add-hook 'elixir-mode-hook
-		  (lambda ()
-			(setq evil-shift-width 2)))
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
+;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; Zero delay suggestions
+(setq which-key-idle-delay 0.3)
+
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; @see https://bitbucket.org/lyro/evil/issue/511/let-certain-minor-modes-key-bindings
+(with-eval-after-load 'git-timemachine
+  (evil-make-overriding-map git-timemachine-mode-map 'normal)
+  ;; force update evil keymaps after git-timemachine-mode loaded
+  (add-hook 'git-timemachine-mode-hook #'evil-normalize-keymaps))
