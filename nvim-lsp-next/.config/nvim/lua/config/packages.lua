368bd0b8fc8c8da738fe7b620a56c6120e74734c
Max Salminen <max.salminen@protonmail.com>
2021-10-30 20:43:56 +0300

    Add dap bindings

--- nvim-lsp-next/.config/nvim/lua/config/packages.lua
+++ nvim-lsp-next/.config/nvim/lua/config/packages.lua
@@ -27,6 +27,11 @@ require('packer').startup(function(use)
     'williamboman/nvim-lsp-installer',
   }
   use {
+    'mfussenegger/nvim-dap',
+    'rcarriga/nvim-dap-ui',
+    'theHamsta/nvim-dap-virtual-text',
+  }
+  use {
     'kyazdani42/nvim-tree.lua',
     requires = 'kyazdani42/nvim-web-devicons',
   }
