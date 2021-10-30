36fd4fc7cb0c0484fc2cf8e554bb825b6434dbd2
Max Salminen <max.salminen@protonmail.com>
2021-10-30 12:32:09 +0300

    Add LSP support

---
+++ nvim-lsp-next/.config/nvim/lua/config/lsp.lua
@@ -0,0 +1,15 @@
+local coq = require('coq')
+local lsp_installer = require("nvim-lsp-installer")
+
+lsp_installer.on_server_ready(function(server)
+    local opts = {}
+
+    -- (optional) Customize the options passed to the server
+    -- if server.name == "tsserver" then
+    --     opts.root_dir = function() ... end
+    -- end
+
+    -- This setup() function is exactly the same as lspconfig's setup function (:help lspconfig-quickstart)
+    server:setup(coq.lsp_ensure_capabilities(opts))
+    vim.cmd [[ do User LspAttachBuffers ]]
+end)
