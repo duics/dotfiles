72a2ba813fb4617c1a66f98078b698ac4593d494
Max Salminen <max.salminen@protonmail.com>
2021-10-31 10:04:51 +0200

    Fine tune dap config

---
+++ nvim-lsp-next/.config/nvim/lua/config/dap.lua
@@ -0,0 +1,45 @@
+vim.g.dap_virtual_text = true
+local dap = require("dap")
+require("dapui").setup()
+
+vim.fn.sign_define('DapBreakpoint', {text='🛑', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointCondition', {text='🔵', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapLogPoint', {text='🟢', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapStopped', {text='⏩', texthl='', linehl='', numhl=''})
+vim.fn.sign_define('DapBreakpointRejected', { text='⛔', texthl='', linehl='', numhl='' })
+
+
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = {os.getenv('HOME') .. '/.local/opt/vscode-node-debug2/out/src/nodeDebug.js'},
+}
+
+dap.configurations.javascript = {
+  {
+    type = 'node2',
+    request = 'launch',
+    program = '${workspaceFolder}/${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+}
+
+dap.configurations.typescript = {
+  {
+		type = 'node2',
+		name = 'Node attach',
+		request = 'attach',
+		port = 9229,
+    program = '${file}',
+		-- remoteRoot = "/home/node/app",
+		-- localRoot = vim.fn.getcwd(),
+		restart = true,
+		sourceMaps = true,
+		-- smartStep = true,
+		-- outFiles = { vim.fn.getcwd() .. "/dist" },
+		-- trace = true,
+  },
+}
