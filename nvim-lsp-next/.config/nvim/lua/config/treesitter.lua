5cf009f1286c8fe1a00194acf1e0ef6c7519632a
Max Salminen <max.salminen@protonmail.com>
2022-05-01 15:37:03 +0300

    Extra langs to treesitter

--- nvim-lsp-next/.config/nvim/lua/config/treesitter.lua
+++ nvim-lsp-next/.config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"typescript", "php", "javascript", "html", "json"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {"typescript", "php", "javascript", "html", "json", "elixir", "eex", "heex"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = { }, -- List of parsers to ignore installing
   highlight = {
     enable = true,
