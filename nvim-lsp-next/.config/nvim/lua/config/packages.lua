e862337d2407fe6f901e063640ec6e0cc0d78bd6
Max Salminen <max.salminen@protonmail.com>
2023-01-12 09:33:06 +0200

    Update ancient nvim-tree-lua reference

--- nvim-lsp-next/.config/nvim/lua/config/packages.lua
+++ nvim-lsp-next/.config/nvim/lua/config/packages.lua
@@ -89,7 +89,7 @@ require('packer').startup(function(use)
   use {'hrsh7th/cmp-nvim-lsp', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline', 'hrsh7th/nvim-cmp', 'hrsh7th/cmp-vsnip', 'hrsh7th/vim-vsnip'  }
   use "rafamadriz/friendly-snippets"
   use {
-    'kyazdani42/nvim-tree.lua',
+    'nvim-tree/nvim-tree.lua',
     requires = {
       'kyazdani42/nvim-web-devicons', -- optional, for file icon
     }
