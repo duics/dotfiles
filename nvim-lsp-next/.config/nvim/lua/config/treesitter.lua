fa97076747270f3af264c2937bf33783c182d083
Max Salminen <max.salminen@protonmail.com>
2023-01-16 19:46:28 +0200

    .tsx is it's own language in treesitter

--- nvim-lsp-next/.config/nvim/lua/config/treesitter.lua
+++ nvim-lsp-next/.config/nvim/lua/config/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = {"typescript", "php", "javascript", "html", "json", "elixir", "eex", "heex", "markdown"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = {"typescript", "tsx", "php", "javascript", "html", "json", "elixir", "eex", "heex", "markdown"}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = { }, -- List of parsers to ignore installing
   highlight = {
     enable = true,
