d06ed72e6d172d2e72582939079ed0d3562633b9
Max Salminen <max.salminen@protonmail.com>
2023-05-02 09:35:25 +0300

    Borders in autocomplete

--- nvim-lsp-next/.config/nvim/init.lua
+++ nvim-lsp-next/.config/nvim/init.lua
@@ -34,8 +34,8 @@ cmp.setup({
     end,
   },
   window = {
-    -- completion = cmp.config.window.bordered(),
-    -- documentation = cmp.config.window.bordered(),
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
   mapping = ({
     ['<tab>'] = function(fallback)
