5d59edc4d5c6d22dd2a24c624f958e4f6cd09d38
Max Salminen <max.salminen@protonmail.com>
2024-02-22 08:55:23 +0200

    Add cargo path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,3 +6,5 @@ end
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+fish_add_path ~/.cargo/bin
