1470f733b73ea71c25cb945ff5621af325ebdee6
Max Salminen <max.salminen@protonmail.com>
2024-08-08 10:18:09 +0300

    Use nvim-surround

--- nvim-lsp-next/.config/nvim/lua/plugins.lua
+++ nvim-lsp-next/.config/nvim/lua/plugins.lua
@@ -1,6 +1,15 @@
 return {
   'wbthomason/packer.nvim',
-  'tpope/vim-surround',
+  {
+      "kylechui/nvim-surround",
+      version = "*", -- Use for stability; omit to use `main` branch for the latest features
+      event = "VeryLazy",
+      config = function()
+          require("nvim-surround").setup({
+              -- Configuration here, or leave empty to use defaults
+          })
+      end
+  },
   'tpope/vim-projectionist',
   'tpope/vim-abolish',
   'mattn/emmet-vim',
