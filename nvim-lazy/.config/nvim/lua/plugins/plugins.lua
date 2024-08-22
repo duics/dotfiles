122417d58eb20e5753332541a7c4d0d29441ca78
Max Salminen <max.salminen@protonmail.com>
2024-08-22 09:57:40 +0300

    Add nvim-lazy config

---
+++ nvim-lazy/.config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        panel = {
+          enabled = true,
+          auto_refresh = true,
+          keymap = {},
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          -- debounce = 75,
+          -- f
+          keymap = {
+            accept = "<a-a>",
+            accept_line = "<a-A>",
+            accept_word = "<a-w>",
+            next = "<a-n>",
+            prev = "<a-p>",
+            dismiss = "<a-c>",
+          },
+        },
+      })
+    end,
+  },
+}
