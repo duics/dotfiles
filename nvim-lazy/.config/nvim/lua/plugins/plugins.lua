9841f975c42d79f6fc825123d238e4de47f543c7
Max Salminen <max.salminen@protonmail.com>
2024-10-21 09:52:14 +0300

    Add Gitlink

--- nvim-lazy/.config/nvim/lua/plugins/plugins.lua
+++ nvim-lazy/.config/nvim/lua/plugins/plugins.lua
@@ -20,6 +20,15 @@ return {
     "tpope/vim-projectionist",
     keys = { { "<leader>a", "<cmd>Alternate<cr>", desc = "Alternative file" } },
   },
+  {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
+      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+    },
+  },
   -- {
   --   "zbirenbaum/copilot.lua",
   --   cmd = "Copilot",
