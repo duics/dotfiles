14918c0c2bfa25adc260cb7aab3a332476d902d7
Max Salminen <max.salminen@protonmail.com>
2024-10-28 14:10:43 +0200

    Add claude.vim

--- nvim-lazy/.config/nvim/lua/plugins/plugins.lua
+++ nvim-lazy/.config/nvim/lua/plugins/plugins.lua
@@ -18,7 +18,7 @@ return {
   },
   {
     "tpope/vim-projectionist",
-    keys = { { "<leader>a", "<cmd>Alternate<cr>", desc = "Alternative file" } },
+    keys = { { "<leader>fa", "<cmd>Alternate<cr>", desc = "Alternative file" } },
   },
   {
     "linrongbin16/gitlinker.nvim",
@@ -43,6 +43,40 @@ return {
       },
     },
   },
+  {
+    "pasky/claude.vim",
+    cmd = {
+      "ClaudeImplement",
+      "ClaudeChat",
+    },
+    keys = {
+      { "<leader>ac", "<cmd>ClaudeChat<cr>", mode = { "n" }, desc = "Claude chat" },
+      { "<leader>ai", "<cmd>ClaudeImplement!<cr>", mode = { "v" }, desc = "Claude implement" },
+    },
+    config = function()
+      local api_key
+      local file = io.open("/home/maxsal/.claude", "r")
+      if file then
+        api_key = file:read()
+        file:close()
+      end
+      if api_key then
+        vim.g.claude_api_key = api_key
+      else
+        vim.notify("API key not found from ~/.claude", vim.log.levels.WARN)
+      end
+    end,
+  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, 1, {
+  --       name = "copilot",
+  --       group_index = 1,
+  --       priority = 100,
+  --     })
+  --   end,
+  -- },
   -- {
   --   "zbirenbaum/copilot.lua",
   --   cmd = "Copilot",
