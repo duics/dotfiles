40dd2ceafc03292f04bc8f2d6b9c23acc3839363
Max Salminen <duics@users.noreply.github.com>
2026-03-12 11:15:49 +0200

    Sync nvim

--- nvim-lazy/.config/nvim/lua/plugins/plugins.lua
+++ nvim-lazy/.config/nvim/lua/plugins/plugins.lua
@@ -26,7 +26,7 @@ return {
     opts = {},
     keys = {
       { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Yank git link" },
-      { "<leader>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
+      { "<leader>gY", "<cmd>GitLink default_branch<cr>", mode = { "n", "v" }, desc = "Yank git link (main)" },
     },
   },
   -- {
@@ -85,33 +85,5 @@ return {
   --       priority = 100,
   --     })
   --   end,
-  -- },
-  -- {
-  --   "zbirenbaum/copilot.lua",
-  --   cmd = "Copilot",
-  --   event = "InsertEnter",
-  --   config = function()
-  --     require("copilot").setup({
-  --       panel = {
-  --         enabled = true,
-  --         auto_refresh = true,
-  --         keymap = {},
-  --       },
-  --       suggestion = {
-  --         enabled = true,
-  --         auto_trigger = true,
-  --         -- debounce = 75,
-  --         -- f
-  --         keymap = {
-  --           accept = "<a-a>",
-  --           accept_line = "<a-A>",
-  --           accept_word = "<a-w>",
-  --           next = "<a-n>",
-  --           prev = "<a-p>",
-  --           dismiss = "<a-c>",
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- },
+  -- }
 }
